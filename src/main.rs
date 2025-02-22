18b734b288d9046253ad956b869182a406fa32ff
Placeless <mikoprpr@hotmail.com>
2025-02-23 01:36:21 +0900

    もう何回もコケてる

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
-use button_driver::{Button, ButtonConfig, State as OtherState};
+use button_driver::{Button, ButtonConfig, State, InstantProvider};
 use esp_idf_hal::{gpio::*, prelude::Peripherals};
 use esp_idf_sys::EspError;
 use log::info;
 use std::time::Duration;
 
-pub enum State<I> {
-    Down( /* … */ ),
-    Pressed( /* … */ ),
-    Up( /* … */ ),
-    Held( /* … */ ),
-    Released,
-    /* … */
-}
 fn main() -> Result<(), EspError> {
     esp_idf_svc::log::EspLogger::initialize_default();
 
@@ -19,7 +11,7 @@ fn main() -> Result<(), EspError> {
     let pin = PinDriver::input(peripherals.pins.gpio35)?;
 
     let mut button =
-        Button::<_, State<I>, Duration>::new(pin, ButtonConfig::default());
+        Button::<_, State::Unknown, Duration>::new(pin, ButtonConfig::default());
 
     loop {
         button.tick();
