06dbfb2fc60da3ac9c14e0bfc1f490934b6eac59
Placeless <mikoprpr@hotmail.com>
2025-02-22 19:19:43 +0900

    struct & impl エーアイに聞く……

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::time::Instant;
 use button_driver::{Button, ButtonConfig};
 use esp_idf_hal::{gpio::PinDriver, prelude::Peripherals};
 use esp_idf_sys::EspError;
@@ -12,13 +11,26 @@ impl Button for Instant {
 }
 */
 
+struct MyPinWrapper<'a, T: InputPin> {
+    pin: PinDriver<'a, T, Input>,
+}
+
+//PinWrapperトレイトをMyPinWrapperに実装する。
+impl<'a, T: InputPin> PinWrapper for MyPinWrapper<'a, T> {
+    //PinWrapperトレイトのメソッドを実装する。
+    fn is_high(&self) -> bool {
+        self.pin.is_high().unwrap_or(false)
+    }
+    //他のPinWrapperトレイトのメソッドを実装する。
+}
+
 fn main() -> Result<(), EspError> {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     let peripherals = Peripherals::take().unwrap();
     let pin = PinDriver::input(peripherals.pins.gpio35)?;
-
-    let mut button = Button::new(pin, ButtonConfig::default());
+    let my_pin = MyPinWrapper { pin };
+    let mut button = Button::new(my_pin, ButtonConfig::default());
 
 loop {
     button.tick();
