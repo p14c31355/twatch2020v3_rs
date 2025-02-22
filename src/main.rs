86f0b56d0aa53af85dd65d21cef72a43efbe7c17
Placeless <mikoprpr@hotmail.com>
2025-02-23 07:14:32 +0900

    オタクドライバはもう使わずに行く❗️

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::time::Instant;
 use std::time::Duration;
 
 use button_driver::{Button, ButtonConfig};
+use esp_idf_hal::gpio;
 use esp_idf_hal::gpio::*;
 use esp_idf_hal::prelude::*;
 use esp_idf_sys::EspError;
@@ -13,37 +14,7 @@ fn main() -> Result<(), EspError> {
     let peripherals = Peripherals::take().unwrap();
     let pin = PinDriver::input(peripherals.pins.gpio35)?;
 
-    let mut button = Button::<_, Instant>::new(pin, ButtonConfig::default());
-
-    loop {
-        button.tick();
-
-        if let Some(held_dur) = button.held_time() {
-            log_button_event(&button, Some(held_dur));
-        } else if let Some(holding_dur) = button.current_holding_time() {
-            info!("Held for {:?}", holding_dur);
-        } else {
-            log_button_event(&button, None);
-        }
-
-        button.reset();
-    }
-}
-
-fn log_button_event(button: &Button<PinDriver<'_, Gpio35, Input>, Instant>, held_dur: Option<Duration>) {
-    let held_str = held_dur.map(|dur| format!(" + held {:?}", dur)).unwrap_or_default();
-
-    if button.is_clicked() {
-        info!("Click{}", held_str);
-    } else if button.is_double_clicked() {
-        info!("Double click{}", held_str);
-    } else if button.is_triple_clicked() {
-        info!("Triple click");
-    } else if button.holds() == 2 {
-        if button.clicks() > 0 {
-            info!("Held twice with {} clicks{}", button.clicks(), held_str);
-        } else {
-            info!("Held twice{}", held_str);
-        }
-    }
+    let mut button: InputPin = gpio::Pull;
+    let mut button1 = pin.is_set_low();
+    Ok(())
 }
