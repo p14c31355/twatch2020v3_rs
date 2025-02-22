68589f0cca91317a8a89000ef59e3544f992b790
Placeless <mikoprpr@hotmail.com>
2025-02-23 00:35:05 +0900

    メールで聞いた方法

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::time::Instant;
-
-use button_driver::{Button, ButtonConfig};
+use button_driver::{Button, ButtonConfig, InstantProvider, State};
 use esp_idf_hal::{gpio::PinDriver, prelude::Peripherals};
 use esp_idf_sys::EspError;
 use log::info;
@@ -9,9 +7,9 @@ fn main() -> Result<(), EspError> {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     let peripherals = Peripherals::take().unwrap();
-    let pin = PinDriver::input(peripherals.pins.gpio9)?;
+    let pin = PinDriver::input(peripherals.pins.gpio35)?;
 
-    let mut button = Button::<_, Instant>::new(pin, ButtonConfig::default());
+    let mut button = Button::<_, State<I>, _>::new(pin, ButtonConfig::default());
 
     loop {
         button.tick();
