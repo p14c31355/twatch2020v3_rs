8e9970735963a934ac4590f5ad657c5f12ee113a
Placeless <mikoprpr@hotmail.com>
2025-02-23 18:42:43 +0900

    nomsg

--- src/main.rs
+++ src/main.rs
@@ -14,38 +14,18 @@ enum State {
 
 fn main() -> Result<(), EspError> {
     EspLogger::initialize_default();
-
-    let peripherals = Peripherals::take().unwrap();
-    let pin = PinDriver::input(peripherals.pins.gpio35)?;
-
-    let mut last_state = match pin.is_low() {
-        Ok(true) => State::On,
-        Ok(false) => State::Off,
-        Err(e) => return Err(e),
-    };
-    let debounce_delay = Duration::from_millis(50);
-
+    let p = Peripherals::take().unwrap();
+    let pin = PinDriver::input(p.pins.gpio35)?;
+    let mut last = if pin.is_low() { State::On } else { State::Off };
     loop {
-        let current_state = match pin.is_low() {
-            Ok(true) => State::On,
-            Ok(false) => State::Off,
-            Err(e) => return Err(e),
-        };
-
-        if current_state != last_state {
-            thread::sleep(debounce_delay);
-            let debounced_state = match pin.is_low() {
-                Ok(true) => State::On,
-                Ok(false) => State::Off,
-                Err(e) => return Err(e),
-            };
-            if debounced_state == current_state {
-                if current_state == State::On {
-                    info!("HelloButton!");
-                }
-                last_state = current_state;
+        let cur = if pin.is_low() { State::On } else { State::Off };
+        if cur != last {
+            thread::sleep(Duration::from_millis(1000));
+            if cur == if pin.is_low() { State::On } else { State::Off } {
+                if cur == State::On { info!("HelloButton!"); }
+                last = cur;
             }
         }
-        thread::sleep(Duration::from_millis(10));
+        thread::sleep(Duration::from_millis(1000));
     }
 }
