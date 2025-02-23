39edcc41648219564e5c72df186511d3fee36644
Placeless <mikoprpr@hotmail.com>
2025-02-24 02:44:29 +0900

    オタクドライバに屈服

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,49 @@
 use esp_idf_hal::prelude::*;
 use esp_idf_hal::gpio::PinDriver;
-use esp_idf_sys::EspError;
-use esp_idf_svc::log::*; // log マクロを使う為に必要
-use log::*; // log levelを使う為に必要
+use esp_idf_sys::EspError; // ?を使用するために必要
+use esp_idf_svc::log::*;
+use log::*;
 use std::time::Duration;
 use std::thread;
+use std::time::Instant;
 
-fn main() -> bool {
-    EspLogger::initialize_default(); // ロギングの初期化
+use button_driver::{Button, ButtonConfig};
+
+fn main() -> Result<(), EspError> {
+    EspLogger::initialize_default();
 
     let peripherals = Peripherals::take().unwrap();
     let pin = PinDriver::input(peripherals.pins.gpio35)?;
 
-    let mut last_state = pin.is_high(); // 初期状態を保存
-    let debounce_delay = Duration::from_millis(50); // チャタリング対策の遅延
+    let mut button = Button::<_, Instant>::new(pin, ButtonConfig::default());
 
     loop {
-        let current_state = pin.is_low(); // 現在の状態を取得
-            if let last_state = current_state {
-                info!("HelloButton!")
-            } 
-        thread::sleep(Duration::from_millis(10)); // 状態確認の間隔
-        } 
-        /*
-        if current_state != last_state {
-            // 状態が変化した場合、チャタリング対策の遅延
-            thread::sleep(debounce_delay);
-            if pin.is_low() == current_state {
-                // 遅延後も状態が変わらなければ、有効な状態変化とみなす
-                if current_state {
-                    info!("HelloButton!"); // ログメッセージを出力
-                }
-                last_state = current_state; // 状態を更新
+        button.tick();
+
+        if let Some(dur) = button.held_time() {
+            info!("Total holding time {:?}", dur);
+
+            if button.is_clicked() {
+                info!("Clicked + held");
+            } else if button.is_double_clicked() {
+                info!("Double clicked + held");
+            } else if button.holds() == 2 && button.clicks() > 0 {
+                info!("Held twice with {} clicks", button.clicks());
+            } else if button.holds() == 2 {
+                info!("Held twice");
+            }
+        } else {
+            if button.is_clicked() {
+                info!("Click");
+            } else if button.is_double_clicked() {
+                info!("Double click");
+            } else if button.is_triple_clicked() {
+                info!("Triple click");
+            } else if let Some(dur) = button.current_holding_time() {
+                info!("Held for {:?}", dur);
             }
         }
-        thread::sleep(Duration::from_millis(10)); // 状態確認の間隔
-        */
+
+        button.reset();
+    }
 }
