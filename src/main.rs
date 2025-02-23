8c3b61294dbff92a9555cc6e9ea02f4d1f4114e4
Placeless <mikoprpr@hotmail.com>
2025-02-23 16:52:59 +0900

    35 = 割り込み，is_low = 押された場合

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 use esp_idf_hal::prelude::*;
 use esp_idf_hal::gpio::PinDriver;
 use esp_idf_sys::EspError;
+use std::time::Duration;
+use std::thread;
 
 fn main() -> Result<(), EspError> {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     let peripherals = Peripherals::take().unwrap();
-    let pin = PinDriver::input(peripherals.pins.gpio22)?;
+    let pin = PinDriver::input(peripherals.pins.gpio35)?;
 
-        if pin.is_high() {
-                    
-        } else {
-            println!("HelloButton!")
+    loop {
+        if pin.is_high() { // ボタンが押されたとき（LOWになる場合）
+            println!("HelloButton!");
+            // ボタンが離されるまで待機
+            while pin.is_high() {
+                thread::sleep(Duration::from_millis(10));
+            }
         }
-        Ok(())
+        thread::sleep(Duration::from_millis(10)); // 状態確認の間隔
+    }
 }
