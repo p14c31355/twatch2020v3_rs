671fc852e5f2e008d88ad7072400e599d5e01bf7
Placeless <mikoprpr@hotmail.com>
2025-02-24 03:02:57 +0900

    コピペバトル……

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,43 @@
-use esp_idf_svc::timer::EspTimer; // 正しいインポート
-use button_driver::{Button, ButtonConfig};
+use esp_idf_svc::timer::EspTimer;
 use esp_idf_hal::{gpio::PinDriver, prelude::Peripherals};
 use esp_idf_sys::EspError;
 use log::info;
+use std::thread;
+use std::time::Duration;
 
 fn main() -> Result<(), EspError> {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     let peripherals = Peripherals::take().unwrap();
     let pin = PinDriver::input(peripherals.pins.gpio35)?;
-
-    let mut button = Button::<_, EspTimer>::new(pin, ButtonConfig::default());
+    let mut last_state = pin.is_high();
+    let debounce_delay = Duration::from_millis(50);
+    let mut last_click_time = EspTimer::new()?;
+    let double_click_interval = Duration::from_millis(300);
+    let mut click_count = 0;
 
     loop {
-    button.tick();
-    if let Some(dur) = button.held_time() {
-            info!("Total holding time {:?}", dur);
-
-            if button.is_clicked() {
-                info!("Clicked + held");
-            } else if button.is_double_clicked() {
-                info!("Double clicked + held");
-            } else if button.holds() == 2 && button.clicks() > 0 {
-                info!("Held twice with {} clicks", button.clicks());
-            } else if button.holds() == 2 {
-                info!("Held twice");
-            }
-        } else {
-            if button.is_clicked() {
-                info!("Click");
-            } else if button.is_double_clicked() {
-                info!("Double click");
-            } else if button.is_triple_clicked() {
-                info!("Triple click");
-            } else if let Some(dur) = button.current_holding_time() {
-                info!("Held for {:?}", dur);
+        let current_state = pin.is_low();
+        if last_state != current_state {
+            thread::sleep(debounce_delay);
+            if pin.is_low() == current_state {
+                last_state = current_state;
+                if current_state {
+                    let now = EspTimer::new()?;
+                    let elapsed = now.elapsed() - last_click_time.elapsed();
+                    if elapsed < double_click_interval{
+                        click_count += 1;
+                        info!("double click");
+                    }else{
+                        click_count = 1;
+                        info!("click");
+                    }
+                    last_click_time = now;
+                }else{
+                    info!("button released");
+                }
             }
         }
-    button.reset();
+        thread::sleep(Duration::from_millis(10));
     }
 }
