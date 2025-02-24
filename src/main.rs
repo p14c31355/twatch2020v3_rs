1865575e38dc0d7fcbd901ba5835ac236e7f95b4
Placeless <mikoprpr@hotmail.com>
2025-02-24 13:53:26 +0900

    tokio::spawn に渡された非同期クロージャ内で MutexGuard が await ポイントを跨いで保持されているために発生しています。MutexGuard は Send トレイトを実装していないため、await ポイントを跨いで保持すると、非同期タスクが別のスレッドに移動した際に問題が発生します。
    原因の詳細:
    
    ThreadSafeButton::update メソッド内で、Mutex をロックして MutexGuard を取得し、その MutexGuard を await ポイントを跨いで使用しています。
    MutexGuard は Send トレイトを実装していないため、await ポイントを跨いで保持すると、非同期タスクが別のスレッドに移動した際に所有権の問題が発生します。
    解決策:
    
    MutexGuard を await ポイントを跨いで保持しないようにコードを修正:
    MutexGuard を取得するスコープを最小限に抑え、await ポイントを跨いで保持しないようにします。
    具体的には、MutexGuard を取得して Button::update を呼び出し、その結果を await ポイントを跨いで使用しないようにします。

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,42 @@ use esp_idf_sys::EspError;
 use log::info;
 use async_button::*;
 use tokio;
+use std::sync::{Arc, Mutex};
+
+struct ThreadSafeButton {
+    button: Arc<Mutex<Button<PinDriver<'static, esp_idf_hal::gpio::Gpio35, esp_idf_hal::gpio::Input>>>>,
+}
+
+impl ThreadSafeButton {
+    fn new(button: Button<PinDriver<'static, esp_idf_hal::gpio::Gpio35, esp_idf_hal::gpio::Input>>) -> Self {
+        ThreadSafeButton {
+            button: Arc::new(Mutex::new(button)),
+        }
+    }
+
+    async fn update(&self) -> ButtonEvent {
+        self.button.lock().unwrap().update().await
+    }
+}
 
 fn main() -> Result<(), EspError> {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     let peripherals = esp_idf_hal::prelude::Peripherals::take().unwrap();
-    let pin = PinDriver::input(peripherals.pins.gpio35)?;
-    let mut button = Button::new(pin, ButtonConfig::default());
+    let pin = PinDriver::input(peripherals.pins.gpio35).unwrap();
+    let button = Button::new(pin, ButtonConfig::default());
+    let safe_button = ThreadSafeButton::new(button);
 
     tokio::spawn(async move {
         loop {
-            match button.update().await {
+            match safe_button.update().await {
                 ButtonEvent::ShortPress { count: _ } => info!("HelloButton!"),
                 ButtonEvent::LongPress => info!("It hurts!"),
             }
         }
     });
 
-    // ここで何か他の処理を行うこともできます。
-    loop{
+    loop {
         std::thread::sleep(std::time::Duration::from_millis(1000));
     }
 }
