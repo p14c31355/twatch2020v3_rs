c7689659bb11564ab0f043bb20e8d52abb831baf
Placeless <mikoprpr@hotmail.com>
2025-02-24 13:51:11 +0900

    tokio の機能は rt と macros でいい

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,26 @@ use esp_idf_hal::gpio::PinDriver;
 use esp_idf_sys::EspError;
 use log::info;
 use async_button::*;
-use tokio; // tokioを追加
+use tokio;
 
-#[tokio::main] // main関数を非同期化
-async fn main() -> Result<(), EspError> {
+fn main() -> Result<(), EspError> {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     let peripherals = esp_idf_hal::prelude::Peripherals::take().unwrap();
     let pin = PinDriver::input(peripherals.pins.gpio35)?;
     let mut button = Button::new(pin, ButtonConfig::default());
 
-    loop {
-        match button.update().await {
-            ButtonEvent::ShortPress { count: _ } => info!("HelloButton!"),
-            ButtonEvent::LongPress => info!("It hurts!"),
+    tokio::spawn(async move {
+        loop {
+            match button.update().await {
+                ButtonEvent::ShortPress { count: _ } => info!("HelloButton!"),
+                ButtonEvent::LongPress => info!("It hurts!"),
+            }
         }
+    });
+
+    // ここで何か他の処理を行うこともできます。
+    loop{
+        std::thread::sleep(std::time::Duration::from_millis(1000));
     }
 }
