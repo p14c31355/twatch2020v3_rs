23ced62081fdc6f334a943ae93aa63318e22b796
Placeless <mikoprpr@hotmail.com>
2025-02-24 13:43:26 +0900

    ESP-IDF 下で fn main は非同期処理に対応していないエラー例

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
-use esp_idf_hal::timer::{Timer, TimerDriver, config::Config};
-use esp_idf_hal::{gpio::PinDriver, prelude::Peripherals};
+use esp_idf_hal::gpio::PinDriver;
 use esp_idf_sys::EspError;
 use log::info;
-use std::thread;
-use std::time::Duration;
 use async_button::*;
+use tokio; // tokioを追加
 
-fn main() -> Result<(), EspError> {
+#[tokio::main] // main関数を非同期化
+async fn main() -> Result<(), EspError> {
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    let peripherals = Peripherals::take().unwrap();
+    let peripherals = esp_idf_hal::prelude::Peripherals::take().unwrap();
     let pin = PinDriver::input(peripherals.pins.gpio35)?;
     let mut button = Button::new(pin, ButtonConfig::default());
 
     loop {
         match button.update().await {
-            ButtonEvent::ShortPress { count } => {info!("HelloButton!")},
-            ButtonEvent::LongPress => {info!("It hurts!")},
+            ButtonEvent::ShortPress { count: _ } => info!("HelloButton!"),
+            ButtonEvent::LongPress => info!("It hurts!"),
         }
     }
 }
