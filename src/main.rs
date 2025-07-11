5963f90e2f67b0f647f60920389a35db9286da40
Placeless <thinkoverheat@gmail.com>
2025-07-12 05:13:07 +0900

    ERROR

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use std::fmt;
 use esp_idf_hal::{
     delay::FreeRtos,
-    gpio::AnyIOPin,
     gpio::PinDriver,
     prelude::*,
     spi::{config::{Config as SpiConfig, DriverConfig}, SpiDeviceDriver, SpiDriver},
@@ -14,46 +12,50 @@ use embedded_graphics::{
 };
 use mipidsi::{Builder, interface::SpiInterface, models::ST7789};
 
-// 表示用のバッファをstatic mutでグローバルに確保
-static mut DISPLAY_BUFFER: [u8; 4096] = [0; 4096];
-
-// 自作のエラーenum
 #[derive(Debug)]
-enum MyError {
-    MipidsiInitError(String),
-    SpiError(String),
-    GpioError(String),
-    Other(anyhow::Error),
+enum Error {
+    Spi(esp_idf_hal::spi::SpiError),
+    Gpio(esp_idf_hal::gpio::GpioError),
+    Mipidsi(mipidsi::builder::InitError<mipidsi::interface::SpiError<esp_idf_hal::spi::SpiError, esp_idf_hal::gpio::GpioError>, core::convert::Infallible>),
+    Draw(mipidsi::interface::SpiError<esp_idf_hal::spi::SpiError, esp_idf_hal::gpio::GpioError>),
 }
 
-impl fmt::Display for MyError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            MyError::MipidsiInitError(e) => write!(f, "Mipidsi init error: {}", e),
-            MyError::SpiError(e) => write!(f, "SPI error: {}", e),
-            MyError::GpioError(e) => write!(f, "GPIO error: {}", e),
-            MyError::Other(e) => write!(f, "Other error: {}", e),
-        }
+impl From<esp_idf_hal::spi::SpiError> for Error {
+    fn from(e: esp_idf_hal::spi::SpiError) -> Self {
+        Error::Spi(e)
+    }
+}
+impl From<esp_idf_hal::gpio::GpioError> for Error {
+    fn from(e: esp_idf_hal::gpio::GpioError) -> Self {
+        Error::Gpio(e)
+    }
+}
+impl From<mipidsi::builder::InitError<mipidsi::interface::SpiError<esp_idf_hal::spi::SpiError, esp_idf_hal::gpio::GpioError>, core::convert::Infallible>> for Error {
+    fn from(e: mipidsi::builder::InitError<mipidsi::interface::SpiError<esp_idf_hal::spi::SpiError, esp_idf_hal::gpio::GpioError>, core::convert::Infallible>) -> Self {
+        Error::Mipidsi(e)
+    }
+}
+impl From<mipidsi::interface::SpiError<esp_idf_hal::spi::SpiError, esp_idf_hal::gpio::GpioError>> for Error {
+    fn from(e: mipidsi::interface::SpiError<esp_idf_hal::spi::SpiError, esp_idf_hal::gpio::GpioError>) -> Self {
+        Error::Draw(e)
     }
 }
 
-impl std::error::Error for MyError {}
-
-impl From<anyhow::Error> for MyError {
-    fn from(e: anyhow::Error) -> Self {
-        MyError::Other(e)
-    }
-}
-
-fn main() -> Result<(), MyError> {
+fn main() -> Result<(), Error> {
     esp_idf_svc::sys::link_patches();
 
     let peripherals = esp_idf_hal::peripherals::Peripherals::take().unwrap();
 
+    // SPIピン設定
     let sclk = peripherals.pins.gpio18;
     let mosi = peripherals.pins.gpio19;
-    let miso: Option<AnyIOPin> = None;
-    let cs: esp_idf_hal::gpio::AnyOutputPin = peripherals.pins.gpio5.into();
+    let miso = None; // TFT_MISO NULL
+    let cs = peripherals.pins.gpio5.into();
+    let dc = PinDriver::output(peripherals.pins.gpio27)?;
+    let mut bl = PinDriver::output(peripherals.pins.gpio15)?;
+
+    // バックライトON
+    bl.set_high()?;
 
     let spi_driver = SpiDriver::new(
         peripherals.spi2,
@@ -61,45 +63,39 @@ fn main() -> Result<(), MyError> {
         mosi,
         miso,
         &DriverConfig::new(),
-    ).map_err(|e| MyError::SpiError(format!("{:?}", e)))?;
+    )?;
 
     let spi_device = SpiDeviceDriver::new(
         spi_driver,
         Some(cs),
         &SpiConfig::new().baudrate(10_000_000u32.Hz().into()),
-    ).map_err(|e| MyError::SpiError(format!("{:?}", e)))?;
-
-    let dc = PinDriver::output(peripherals.pins.gpio27)
-        .map_err(|e| MyError::GpioError(format!("{:?}", e)))?;
-    let mut bl = PinDriver::output(peripherals.pins.gpio15)
-        .map_err(|e| MyError::GpioError(format!("{:?}", e)))?;
-    bl.set_high().map_err(|e| MyError::GpioError(format!("{:?}", e)))?;
+    )?;
 
     let mut delay = FreeRtos;
 
-    // unsafeブロックでstatic mutを参照
-    let di = unsafe {
-    let raw_ptr: *mut [u8; 4096] = &raw mut DISPLAY_BUFFER;
-    SpiInterface::new(spi_device, dc, &mut *raw_ptr)
-};
+    // バッファは十分大きく(240*240/8=7200バイトより多めに)
+    let mut display_buffer = [0u8; 4096];
+    let di = SpiInterface::new(spi_device, dc, &mut display_buffer);
 
+    // RSTピンなしの設定で初期化
     let mut display = Builder::new(ST7789, di)
         .display_size(240, 240)
-        .init(&mut delay)
-        .map_err(|e| MyError::MipidsiInitError(format!("{:?}", e)))?;
+        .reset_pin(None)  // RSTピンは接続していないためNone
+        .rotation(mipidsi::Rotation::Rotate0)
+        .init(&mut delay)?;
 
-    display.clear(Rgb565::BLACK)
-        .map_err(|e| MyError::SpiError(format!("{:?}", e)))?;
+    delay.delay_ms(100); // 初期化後の余裕
+
+    display.clear(Rgb565::BLACK)?;
 
     let style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
 
     Text::new("Hello TWatch 2020 V3!", Point::new(10, 120), style)
-        .draw(&mut display)
-        .map_err(|e| MyError::SpiError(format!("{:?}", e)))?;
+        .draw(&mut display)?;
 
     println!("Display initialized and text drawn!");
 
     loop {
-        FreeRtos::delay_ms(1000);
+        delay.delay_ms(1000);
     }
 }
