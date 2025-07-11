d43dbf02a0d89ee0664d3955cceff26b94ac9eb9
Placeless <thinkoverheat@gmail.com>
2025-07-12 04:43:10 +0900

    デバッグコード追加

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,69 @@
-use esp_idf_hal::gpio::AnyIOPin;
-
 use esp_idf_hal::{
-    delay::FreeRtos, gpio::{AnyOutputPin, PinDriver}, prelude::*, spi::{config::{Config as SpiConfig, DriverConfig}, SpiDeviceDriver, SpiDriver}
+    delay::FreeRtos,
+    gpio::PinDriver,
+    prelude::*,
+    spi::{config::{Config as SpiConfig, DriverConfig}, SpiDeviceDriver, SpiDriver},
 };
-
 use embedded_graphics::{
     mono_font::{ascii::FONT_6X10, MonoTextStyle},
     pixelcolor::Rgb565,
     prelude::*,
     text::Text,
 };
-
-use mipidsi::{models::ST7789, Builder};
-use mipidsi::interface::SpiInterface;
-use mipidsi::options::ColorInversion;
+use mipidsi::{Builder, interface::SpiInterface, models::ST7789};
 use anyhow::Result;
 
-// embedded-hal v0.2互換用 ダミーDCピン定義
-use embedded_hal::digital::OutputPin;
-use core::convert::Infallible;
-
-struct DummyNoopPin;
-
-impl embedded_hal::digital::ErrorType for DummyNoopPin {
-    type Error = Infallible;
-}
-
-impl OutputPin for DummyNoopPin {
-    fn set_low(&mut self) -> Result<(), Self::Error> {
-        Ok(())
-    }
-
-    fn set_high(&mut self) -> Result<(), Self::Error> {
-        Ok(())
-    }
-}
-
-// SPIインタフェース用バッファ
 static mut DISPLAY_BUFFER: [u8; 256] = [0u8; 256];
 
 fn main() -> Result<()> {
     esp_idf_svc::sys::link_patches();
 
-    let peripherals = Peripherals::take().unwrap();
+    let peripherals = esp_idf_hal::peripherals::Peripherals::take().unwrap();
 
     let sclk = peripherals.pins.gpio18;
-    let sdo  = peripherals.pins.gpio19;
-    let sdi: Option<AnyIOPin> = None;
-    let cs: AnyOutputPin = peripherals.pins.gpio5.into();
+    let mosi = peripherals.pins.gpio19;
+    let cs = peripherals.pins.gpio5.into();
 
     let spi_driver = SpiDriver::new(
         peripherals.spi2,
         sclk,
-        sdo,
-        sdi,
+        mosi,
+        None,
         &DriverConfig::new(),
     )?;
 
     let spi_device = SpiDeviceDriver::new(
         spi_driver,
         Some(cs),
-        &SpiConfig::new().baudrate(20.MHz().into()),
+        &SpiConfig::new().baudrate(10_000_000u32.Hz().into()),
     )?;
 
     let dc = PinDriver::output(peripherals.pins.gpio27)?;
     let mut bl = PinDriver::output(peripherals.pins.gpio15)?;
     bl.set_high()?; // バックライトON
 
-    let di = unsafe {
-    SpiInterface::new(spi_device, dc, &mut DISPLAY_BUFFER)
-};
-
-
     let mut delay = FreeRtos;
+
+    let di = unsafe {
+        SpiInterface::new(spi_device, dc, &mut DISPLAY_BUFFER)
+    };
 
     let mut display = Builder::new(ST7789, di)
         .display_size(240, 240)
-        .invert_colors(ColorInversion::Inverted)
-        .init(&mut delay)
-        .unwrap();
+        //.invert_colors(ColorInversion::Inverted) // 外す
+        .init(&mut delay)?;
 
-    display.clear(Rgb565::BLACK).unwrap();
+    display.clear(Rgb565::BLACK)?;
 
-    let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+    let style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
 
-    Text::new("Hello TWatch 2020 V3!", Point::new(20, 120), text_style)
-        .draw(&mut display)
-        .unwrap();
+    Text::new("Hello TWatch 2020 V3!", Point::new(10, 120), style)
+        .draw(&mut display)?;
 
-    println!("Display initialized!");
+    println!("Display initialized and text drawn!");
 
-    let button = PinDriver::input(peripherals.pins.gpio0)?;
-
+    // ボタン監視などなしで無限ループ（省略も可）
     loop {
-        if button.is_low() {
-            println!("Button pressed!");
-        }
-        FreeRtos::delay_ms(100);
+        FreeRtos::delay_ms(1000);
     }
 }
