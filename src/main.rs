e96aaaff43c0e08d842363d50bb3d1ff7ad002e5
Placeless <thinkoverheat@gmail.com>
2025-09-06 05:39:39 +0900

    cargo add axp20x

--- src/main.rs
+++ src/main.rs
@@ -10,35 +10,25 @@ fn main() -> Result<()> {
     let p = Peripherals::take().unwrap();
 
     // I2C 初期化
-    let i2c_cfg = I2cConfig::new().baudrate(100_000.Hz());
+    let i2c_cfg = I2cConfig::new().baudrate(50_000.Hz());
     let mut i2c = I2cDriver::new(p.i2c0, p.pins.gpio21, p.pins.gpio22, &i2c_cfg)?;
 
     let axp_addr = 0x35u8;
+    let mut prev_regs = [0u8; 256];
 
     loop {
-        // LDO2 (REG0x23)
-        let mut buf = [0u8; 1];
-        i2c.read(axp_addr, &mut buf, 0x23)?;
-        let ldo2_setting = buf[0];
-        let ldo2_mv = 700 + (ldo2_setting as u32) * 25;
+        for reg in 0x00u8..=0xFF {
+            let mut buf = [0u8; 1];
+            if i2c.read(axp_addr, &mut buf, reg.into()).is_ok() {
+                if buf[0] != prev_regs[reg as usize] {
+                    println!("REG0x{:02X}: 0x{:02X} -> 0x{:02X}", reg, prev_regs[reg as usize], buf[0]);
+                    prev_regs[reg as usize] = buf[0];
+                }
+            } else {
+                println!("REG0x{:02X}: read error!", reg);
+            }
+        }
 
-        // LDO3 (REG0x24)
-        i2c.read(axp_addr, &mut buf, 0x24)?;
-        let ldo3_setting = buf[0];
-        let ldo3_mv = 700 + (ldo3_setting as u32) * 25;
-
-        // DCDC1 (REG0x26)
-        i2c.read(axp_addr, &mut buf, 0x26)?;
-        let dcdc1_setting = buf[0];
-        let dcdc1_mv = 700 + (dcdc1_setting as u32) * 25;
-
-        println!(
-            "LDO2: 0x{:02X} (~{} mV), LDO3: 0x{:02X} (~{} mV), DCDC1: 0x{:02X} (~{} mV)",
-            ldo2_setting, ldo2_mv,
-            ldo3_setting, ldo3_mv,
-            dcdc1_setting, dcdc1_mv
-        );
-
-        thread::sleep(Duration::from_secs(1));
+        thread::sleep(Duration::from_millis(500));
     }
 }
