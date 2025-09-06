424d22e502cd6e6577c14359834cd9b379d40d02
Placeless <thinkoverheat@gmail.com>
2025-09-06 20:48:34 +0900

    fuck

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,9 @@
 // src/app.rs
 use anyhow::Result;
 use crate::drivers::{axp::PowerManager, display::TwatchDisplay, touch::Touch};
-use embedded_graphics::{mono_font::{MonoTextStyle, ascii::FONT_6X10}, text::Text, pixelcolor::Rgb565, Drawable, prelude::{Point, DrawTarget, RgbColor}};
+use embedded_graphics::{
+    Drawable, mono_font::{MonoTextStyle, ascii::FONT_6X10}, pixelcolor::Rgb565, prelude::{DrawTarget, Point, RgbColor}, text::Text
+};
 use esp_idf_hal::delay::FreeRtos;
 use chrono::{NaiveTime, Timelike};
 use esp_idf_hal::i2c::I2cDriver;
@@ -14,101 +16,84 @@ pub enum AppState {
 }
 
 pub struct App<'a> {
-    i2c: I2cDriver<'a>,
+    i2c: &'a mut I2cDriver<'a>,
     display: TwatchDisplay<'a>,
     state: AppState,
 }
 
-impl<'a> App<'a> {
-    pub fn new(i2c: I2cDriver<'a>, display: TwatchDisplay<'a>) -> Self {
-        Self {
-            i2c,
-            display,
-            state: AppState::Launcher,
-        }
+impl<'a> App<'a> { // Added lifetime parameter for 'a
+    pub fn new(i2c: &'a mut I2cDriver<'a>, display: TwatchDisplay<'a>) -> Self {
+        Self { i2c: i2c, display, state: AppState::Launcher }
     }
 
     pub fn run(&mut self, _delay: &mut FreeRtos) -> Result<()> {
         loop {
             self.draw_status_bar()?;
-
             match self.state {
                 AppState::Launcher => self.show_launcher()?,
                 AppState::Settings => self.show_settings()?,
-                AppState::Battery  => self.show_battery()?,
+                AppState::Battery => self.show_battery()?,
             }
         }
     }
 
     fn draw_status_bar(&mut self) -> Result<()> {
         let now = NaiveTime::from_hms_opt(12, 34, 0).unwrap();
-        let mut power = PowerManager::new(&mut self.i2c)?;
-        let battery = power.axp.get_battery_percentage().map_err(|e| anyhow::anyhow!("{:?}", e))?;
-        let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
-
-        Text::new(&format!("{:02}:{:02}", now.hour(), now.minute()), Point::new(5, 5), text_style)
-            .draw(&mut self.display.display)
-            .map_err(|e| anyhow::anyhow!("{:?}", e))?;
-
-        Text::new(&format!("{}%", battery), Point::new(180, 5), text_style)
-            .draw(&mut self.display.display)
-            .map_err(|e| anyhow::anyhow!("{:?}", e))?;
-
+        {
+            let mut power = PowerManager::new(self.i2c)?;
+            let battery = power.read_voltage()?;
+            let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+            Text::new(&format!("{:02}:{:02}", now.hour(), now.minute()), Point::new(5, 5), text_style)
+                .draw(&mut self.display.display);
+            Text::new(&format!("{:.0}%", battery), Point::new(180, 5), text_style)
+                .draw(&mut self.display.display);
+        }
         Ok(())
     }
 
     fn show_launcher(&mut self) -> Result<()> {
-        let mut touch = Touch::new_with_ref(&mut self.i2c)?;
-        self.display.display.clear(Rgb565::BLACK).map_err(|e| anyhow::anyhow!("{:?}", e))?;
-        let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
-        Text::new("Launcher: tap for apps", Point::new(10, 40), text_style)
-            .draw(&mut self.display.display)
-            .map_err(|e| anyhow::anyhow!("{:?}", e))?;
-
-        if let Some(event) = touch.read_event()? {
-            if event.on_button1() {
-                self.state = AppState::Settings;
-            } else if event.on_button2() {
-                self.state = AppState::Battery;
+        {
+            let mut touch = Touch::new_with_ref(self.i2c)?;
+            if let Some(event) = touch.read_event()? {
+                if event.on_button1() { self.state = AppState::Settings; }
+                else if event.on_button2() { self.state = AppState::Battery; }
             }
         }
+        self.display.display.clear(Rgb565::BLACK);
+        let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+        Text::new("Launcher: tap for apps", Point::new(10, 40), text_style)
+            .draw(&mut self.display.display);
         FreeRtos::delay_ms(50);
         Ok(())
     }
 
     fn show_settings(&mut self) -> Result<()> {
-        let mut touch = Touch::new_with_ref(&mut self.i2c)?;
-        self.display.display.clear(Rgb565::BLACK).map_err(|e| anyhow::anyhow!("{:?}", e))?;
-        let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
-        Text::new("Settings", Point::new(10, 40), text_style)
-            .draw(&mut self.display.display)
-            .map_err(|e| anyhow::anyhow!("{:?}", e))?;
-
-        if let Some(event) = touch.read_event()? {
-            if event.on_back() {
-                self.state = AppState::Launcher;
+        {
+            let mut touch = Touch::new_with_ref(self.i2c)?;
+            if let Some(event) = touch.read_event()? {
+                if event.on_back() { self.state = AppState::Launcher; }
             }
         }
+        self.display.display.clear(Rgb565::BLACK);
+        let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+        Text::new("Settings", Point::new(10, 40), text_style)
+            .draw(&mut self.display.display);
         FreeRtos::delay_ms(50);
         Ok(())
     }
 
     fn show_battery(&mut self) -> Result<()> {
-        let voltage = {
-            let mut power = PowerManager::new(&mut self.i2c)?;
-            power.read_voltage()?
-        };
-        self.display.display.clear(Rgb565::BLACK).map_err(|e| anyhow::anyhow!("{:?}", e))?;
-        let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
-        Text::new(&format!("Battery: {} mV", voltage), Point::new(10, 40), text_style)
-            .draw(&mut self.display.display)
-            .map_err(|e| anyhow::anyhow!("{:?}", e))?;
-
-        let mut touch = Touch::new_with_ref(&mut self.i2c)?;
-        if let Some(event) = touch.read_event()? {
-            if event.on_back() {
-                self.state = AppState::Launcher;
-            }
+        {
+            let mut power = PowerManager::new(self.i2c)?;
+            let voltage = power.read_voltage()?;
+            self.display.display.clear(Rgb565::BLACK);
+            let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+            Text::new(&format!("Battery: {:.2} V", voltage / 1000.0), Point::new(10, 40), text_style)
+                .draw(&mut self.display.display);
+        }
+        let mut touch = Touch::new_with_ref(self.i2c)?;
+        if let Some(event) = touch.read_event()? { // This line was causing the issue
+            if event.on_back() { self.state = AppState::Launcher; }
         }
         FreeRtos::delay_ms(200);
         Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 // src/main.rs
 mod app;
 mod drivers;
+
 use anyhow::Result;
+use esp_idf_hal::prelude::*;
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::i2c::{I2cConfig, I2cDriver};
-use esp_idf_hal::prelude::*;
 use drivers::display::TwatchDisplay;
 use app::App;
 
@@ -14,8 +15,8 @@ fn main() -> Result<()> {
 
     let peripherals = Peripherals::take().unwrap();
     let mut delay = FreeRtos;
-    let i2c_cfg = I2cConfig::new().baudrate(400_000.Hz());
 
+    let i2c_cfg = I2cConfig::new().baudrate(400_000.Hz());
     let i2c_driver = I2cDriver::new(
         peripherals.i2c0,
         peripherals.pins.gpio21,
@@ -23,7 +24,10 @@ fn main() -> Result<()> {
         &i2c_cfg,
     )?;
 
-    let mut display_buffer = [0_u8; 240 * 240 * 2];
+    let i2c_driver: &'static mut I2cDriver = Box::leak(Box::new(i2c_driver));
+
+    let mut display_buffer = Box::leak(Box::new([0_u8; 240 * 240 * 2]));
+
     let display = TwatchDisplay::new(
         peripherals.spi2,
         peripherals.pins.gpio18,
@@ -31,11 +35,10 @@ fn main() -> Result<()> {
         peripherals.pins.gpio5,
         peripherals.pins.gpio27,
         peripherals.pins.gpio33,
-        &mut display_buffer,
+        &mut display_buffer[..],
     )?;
 
     let mut app = App::new(i2c_driver, display);
-
     app.run(&mut delay)?;
     Ok(())
 }
