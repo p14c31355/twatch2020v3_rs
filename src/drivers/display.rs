c6e3e8effa917a468ff84d65737f08c5f6c40f90
Placeless <thinkoverheat@gmail.com>
2025-09-06 17:17:05 +0900

    ok

--- src/drivers/display.rs
+++ src/drivers/display.rs
@@ -1,39 +1,37 @@
 use anyhow::Result;
 use esp_idf_hal::spi::{config::DriverConfig as SpiDriverConfig, config::Config as SpiConfig, SpiDeviceDriver, SpiDriver};
-use esp_idf_hal::gpio::{AnyIOPin, Output, PinDriver, Gpio27, Gpio33, Gpio19, Gpio18, Gpio23, Gpio5};
+use esp_idf_hal::gpio::{AnyIOPin, Output, PinDriver, Gpio27, Gpio33};
 use esp_idf_hal::peripherals::Peripherals;
 use mipidsi::{Builder, models::ST7789, Display};
 use mipidsi::interface::SpiInterface;
-use embedded_graphics::pixelcolor::Rgb565;
 use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::units::FromValueType;
 use mipidsi::options::ColorOrder;
 
-pub type TwatchDisplay<'a> = Display<SpiInterface<'a, SpiDeviceDriver<'a, &'a SpiDriver<'a>>, PinDriver<'a, Gpio27, Output>>, ST7789, PinDriver<'a, Gpio33, Output>>;
+// The SpiDeviceDriver now owns the SpiDriver.
+pub type TwatchDisplay<'a> = Display<SpiInterface<'a, SpiDeviceDriver<'a, SpiDriver<'a>>, PinDriver<'a, Gpio27, Output>>, ST7789, PinDriver<'a, Gpio33, Output>>;
 
 pub fn init_display<'a>(p: Peripherals, buffer: &'a mut [u8]) -> Result<TwatchDisplay<'a>> {
-    let sclk_pin = PinDriver::output(p.pins.gpio18)?.into_any_io_pin();
-    let mosi_pin = PinDriver::output(p.pins.gpio23)?.into_any_io_pin();
-    let miso_pin = PinDriver::input(p.pins.gpio19)?.into_any_io_pin();
-
+    // Create the SpiDriver on the stack.
     let driver = SpiDriver::new(
         p.spi2,
-        sclk_pin,
-        mosi_pin,
-        Some(miso_pin),
+        p.pins.gpio18,
+        p.pins.gpio23,
+        None::<AnyIOPin>,
         &SpiDriverConfig::new(),
+    )?;
+
+    // Move the driver into the SpiDeviceDriver.
+    let spi_device = SpiDeviceDriver::new(
+        driver,
+        Some(p.pins.gpio5),
+        &SpiConfig::new().baudrate(26.MHz().into()),
     )?;
 
     let dc: PinDriver<'_, Gpio27, Output> = PinDriver::output(p.pins.gpio27)?;
     let rst: PinDriver<'_, Gpio33, Output> = PinDriver::output(p.pins.gpio33)?;
-    let cs = PinDriver::output(p.pins.gpio5)?;
 
-    let spi_device = SpiDeviceDriver::new(
-        &driver,
-        Some(cs.into()),
-        &SpiConfig::new().baudrate(26.MHz().into()),
-    )?;
-
+    // Move the spi_device into the SpiInterface.
     let spi = SpiInterface::new(spi_device, dc, buffer);
 
     let display = Builder::new(ST7789, spi)
