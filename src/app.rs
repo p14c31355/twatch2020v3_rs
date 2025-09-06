d68ce63cfbf5f1faff8152c7dff4c9f354955178
Placeless <thinkoverheat@gmail.com>
2025-09-06 19:16:58 +0900

    borrow error

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,9 @@
 // src/app.rs
 use anyhow::Result;
 use crate::drivers::{axp::PowerManager, display::TwatchDisplay, touch::Touch};
-use embedded_graphics::{mono_font::{MonoTextStyle, ascii::FONT_6X10}, text::Text, pixelcolor::Rgb565, Drawable, prelude::{Point, DrawTarget}};
+use embedded_graphics::{mono_font::{MonoTextStyle, ascii::FONT_6X10}, text::Text, pixelcolor::Rgb565, Drawable, prelude::{Point, DrawTarget, RgbColor}};
 use esp_idf_hal::delay::FreeRtos;
-use chrono::NaiveTime;
+use chrono::{NaiveTime, Timelike};
 
 #[derive(Debug)]
 pub enum AppState {
@@ -17,7 +17,6 @@ pub struct App<'a> {
     display: TwatchDisplay<'a>,
     touch: Touch<'a, &'a mut esp_idf_hal::i2c::I2cDriver<'a>>,
     state: AppState,
-    _i2c: esp_idf_hal::i2c::I2cDriver<'a>,
 }
 
 impl<'a> App<'a> {
@@ -34,49 +33,41 @@ impl<'a> App<'a> {
         }
     }
 
-    pub fn new_with_i2c(mut i2c: esp_idf_hal::i2c::I2cDriver<'a>, display: TwatchDisplay<'a>) -> Self {
-        let power = PowerManager::new(&mut i2c).unwrap();
-        let touch = Touch::new_with_ref(&mut i2c).unwrap();
-        Self {
-            power,
-            touch,
-            display,
-            state: AppState::Launcher,
-            _i2c: i2c, // i2c の所有権を移動
-        }
-    }
-
-    pub fn run(&mut self, delay: &mut FreeRtos) -> Result<()> {
+    pub fn run(&mut self, _delay: &mut FreeRtos) -> Result<()> {
         loop {
             self.draw_status_bar()?;
 
             match self.state {
-                AppState::Launcher => self.show_launcher(delay)?,
-                AppState::Settings => self.show_settings(delay)?,
-                AppState::Battery  => self.show_battery(delay)?,
+                AppState::Launcher => self.show_launcher()?,
+                AppState::Settings => self.show_settings()?,
+                AppState::Battery  => self.show_battery()?,
             }
         }
     }
 
     fn draw_status_bar(&mut self) -> Result<()> {
         let now = NaiveTime::from_hms_opt(12, 34, 0).unwrap();
-
         let battery = self.power.axp.get_battery_percentage().map_err(|e| anyhow::anyhow!("{:?}", e))?;
+        let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
 
-        let text_style = MonoTextStyle::new(&FONT_6X10, <Rgb565 as embedded_graphics::prelude::RgbColor>::WHITE);
+        Text::new(&format!("{:02}:{:02}", now.hour(), now.minute()), Point::new(5, 5), text_style)
+            .draw(&mut self.display.display)
+            .map_err(|e| anyhow::anyhow!("{:?}", e))?;
 
-        Text::new(&format!("{:02}:{:02}", chrono::Timelike::hour(&now), chrono::Timelike::minute(&now)), Point::new(5, 5), text_style).draw(&mut self.display.display).map_err(|e| anyhow::anyhow!("{:?}", e))?;
-        Text::new(&format!("{}%", battery), embedded_graphics::geometry::Point::new(180, 5), text_style).draw(&mut self.display.display).map_err(|e| anyhow::anyhow!("{:?}", e))?;
-
+        Text::new(&format!("{}%", battery), Point::new(180, 5), text_style)
+            .draw(&mut self.display.display)
+            .map_err(|e| anyhow::anyhow!("{:?}", e))?;
 
         Ok(())
     }
 
-    fn show_launcher(&mut self, delay: &mut FreeRtos) -> Result<()> {
-        self.display.display.clear(<Rgb565 as embedded_graphics::prelude::RgbColor>::BLACK).map_err(|e| anyhow::anyhow!("{:?}", e))?;
-        let text_style = MonoTextStyle::new(&FONT_6X10, <Rgb565 as embedded_graphics::prelude::RgbColor>::WHITE);
-        Text::new("Launcher: tap for apps", embedded_graphics::geometry::Point::new(10, 40), text_style).draw(&mut self.display.display).map_err(|e| anyhow::anyhow!("{:?}", e))?;
-        
+    fn show_launcher(&mut self) -> Result<()> {
+        self.display.display.clear(Rgb565::BLACK).map_err(|e| anyhow::anyhow!("{:?}", e))?;
+        let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+        Text::new("Launcher: tap for apps", Point::new(10, 40), text_style)
+            .draw(&mut self.display.display)
+            .map_err(|e| anyhow::anyhow!("{:?}", e))?;
+
         if let Some(event) = self.touch.read_event()? {
             if event.on_button1() {
                 self.state = AppState::Settings;
@@ -84,15 +75,16 @@ impl<'a> App<'a> {
                 self.state = AppState::Battery;
             }
         }
-
         FreeRtos::delay_ms(50);
         Ok(())
     }
 
-    fn show_settings(&mut self, delay: &mut FreeRtos) -> Result<()> {
-        self.display.display.clear(<Rgb565 as embedded_graphics::prelude::RgbColor>::BLACK).map_err(|e| anyhow::anyhow!("{:?}", e))?;
-        let text_style = MonoTextStyle::new(&FONT_6X10, <Rgb565 as embedded_graphics::prelude::RgbColor>::WHITE);
-        Text::new("Settings", Point::new(10, 40), text_style).draw(&mut self.display.display).map_err(|e| anyhow::anyhow!("{:?}", e))?;
+    fn show_settings(&mut self) -> Result<()> {
+        self.display.display.clear(Rgb565::BLACK).map_err(|e| anyhow::anyhow!("{:?}", e))?;
+        let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+        Text::new("Settings", Point::new(10, 40), text_style)
+            .draw(&mut self.display.display)
+            .map_err(|e| anyhow::anyhow!("{:?}", e))?;
 
         if let Some(event) = self.touch.read_event()? {
             if event.on_back() {
@@ -103,11 +95,13 @@ impl<'a> App<'a> {
         Ok(())
     }
 
-    fn show_battery(&mut self, delay: &mut FreeRtos) -> Result<()> {
-        self.display.display.clear(<Rgb565 as embedded_graphics::prelude::RgbColor>::BLACK).map_err(|e| anyhow::anyhow!("{:?}", e))?;
+    fn show_battery(&mut self) -> Result<()> {
+        self.display.display.clear(Rgb565::BLACK).map_err(|e| anyhow::anyhow!("{:?}", e))?;
         let voltage = self.power.read_voltage()?;
-        let text_style = MonoTextStyle::new(&FONT_6X10, <Rgb565 as embedded_graphics::prelude::RgbColor>::WHITE); // This line was already present
-        Text::new(&format!("Battery: {} mV", voltage), embedded_graphics::geometry::Point::new(10, 40), text_style).draw(&mut self.display.display).map_err(|e| anyhow::anyhow!("{:?}", e))?;
+        let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+        Text::new(&format!("Battery: {} mV", voltage), Point::new(10, 40), text_style)
+            .draw(&mut self.display.display)
+            .map_err(|e| anyhow::anyhow!("{:?}", e))?;
 
         if let Some(event) = self.touch.read_event()? {
             if event.on_back() {
--- src/main.rs
+++ src/main.rs
@@ -15,29 +15,28 @@ fn main() -> Result<()> {
     let peripherals = Peripherals::take().unwrap();
     let mut delay = FreeRtos;
     let i2c_cfg = I2cConfig::new().baudrate(400_000.Hz());
-    let i2c_driver = esp_idf_hal::i2c::I2cDriver::new(
-    peripherals.i2c0,
-    peripherals.pins.gpio21,
-    peripherals.pins.gpio22,
-    &i2c_cfg,
-)?;
-    // Create a mutable reference to i2c_driver for PowerManager and Touch
-    let mut i2c_driver_ref = i2c_driver;
-    let mut power = PowerManager::new(&mut i2c_driver_ref)?;
-    let mut touch = Touch::new_with_ref(&mut i2c_driver_ref)?;
+    let mut i2c_driver = esp_idf_hal::i2c::I2cDriver::new(
+        peripherals.i2c0,
+        peripherals.pins.gpio21,
+        peripherals.pins.gpio22,
+        &i2c_cfg,
+    )?;
+
+    let power = PowerManager::new(&mut i2c_driver)?;
+    let touch = Touch::new_with_ref(&mut i2c_driver)?;
 
     let mut display_buffer = [0_u8; 240 * 240 * 2];
-let display = TwatchDisplay::new(
-    peripherals.spi2,
-    peripherals.pins.gpio18,
-    peripherals.pins.gpio23,
-    peripherals.pins.gpio5,
-    peripherals.pins.gpio27,
-    peripherals.pins.gpio33,
-    &mut display_buffer,
-)?;
+    let display = TwatchDisplay::new(
+        peripherals.spi2,
+        peripherals.pins.gpio18,
+        peripherals.pins.gpio23,
+        peripherals.pins.gpio5,
+        peripherals.pins.gpio27,
+        peripherals.pins.gpio33,
+        &mut display_buffer,
+    )?;
 
-    let mut app = App::new_with_i2c(i2c_driver, display);
+    let mut app = App::new(power, display, touch);
 
     app.run(&mut delay)?;
     Ok(())
