84b11650acaa3e7a053f40838141db33287c4bd1
Placeless <thinkoverheat@gmail.com>
2025-09-06 21:02:01 +0900

    error

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,11 @@
 // src/app.rs
 use anyhow::Result;
 use crate::drivers::{axp::PowerManager, display::TwatchDisplay, touch::Touch};
-use embedded_graphics::{
-    Drawable, mono_font::{MonoTextStyle, ascii::FONT_6X10}, pixelcolor::Rgb565, prelude::{DrawTarget, Point, RgbColor}, text::Text
-};
+use embedded_graphics::{pixelcolor::Rgb565,
+    Drawable, mono_font::{MonoTextStyle, ascii::FONT_6X10}, prelude::{DrawTarget, Point, RgbColor}, text::Text,
+}; 
 use esp_idf_hal::delay::FreeRtos;
-use chrono::{NaiveTime, Timelike};
-use esp_idf_hal::i2c::I2cDriver;
+use chrono::{NaiveTime, Timelike}; // Add this line
 
 #[derive(Debug)]
 pub enum AppState {
@@ -16,14 +15,15 @@ pub enum AppState {
 }
 
 pub struct App<'a> {
-    i2c: &'a mut I2cDriver<'a>,
+    power_manager: PowerManager<'a>,
+    touch: Touch<'a>,
     display: TwatchDisplay<'a>,
     state: AppState,
 }
 
-impl<'a> App<'a> { // Added lifetime parameter for 'a
-    pub fn new(i2c: &'a mut I2cDriver<'a>, display: TwatchDisplay<'a>) -> Self {
-        Self { i2c: i2c, display, state: AppState::Launcher }
+impl<'a> App<'a> {
+    pub fn new(power_manager: PowerManager<'a>, touch: Touch<'a>, display: TwatchDisplay<'a>) -> Self {
+        Self { power_manager, touch, display, state: AppState::Launcher }
     }
 
     pub fn run(&mut self, _delay: &mut FreeRtos) -> Result<()> {
@@ -38,27 +38,23 @@ impl<'a> App<'a> { // Added lifetime parameter for 'a
     }
 
     fn draw_status_bar(&mut self) -> Result<()> {
-        let now = NaiveTime::from_hms_opt(12, 34, 0).unwrap();
-        {
-            let mut power = PowerManager::new(self.i2c)?;
-            let battery = power.read_voltage()?;
-            let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
-            Text::new(&format!("{:02}:{:02}", now.hour(), now.minute()), Point::new(5, 5), text_style)
-                .draw(&mut self.display.display);
-            Text::new(&format!("{:.0}%", battery), Point::new(180, 5), text_style)
-                .draw(&mut self.display.display);
-        }
+        let now = NaiveTime::from_hms_opt(12, 34, 0).unwrap(); // Placeholder for actual time        
+        let battery = self.power_manager.read_voltage()?;
+        let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+        Text::new(&format!("{:02}:{:02}", now.hour(), now.minute()), Point::new(5, 5), text_style)
+            .draw(&mut self.display.display);
+        Text::new(&format!("{:.0}%", battery), Point::new(180, 5), text_style)
+            .draw(&mut self.display.display);
+
         Ok(())
     }
 
     fn show_launcher(&mut self) -> Result<()> {
-        {
-            let mut touch = Touch::new_with_ref(self.i2c)?;
+            let mut touch = &mut self.touch;
             if let Some(event) = touch.read_event()? {
                 if event.on_button1() { self.state = AppState::Settings; }
                 else if event.on_button2() { self.state = AppState::Battery; }
             }
-        }
         self.display.display.clear(Rgb565::BLACK);
         let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
         Text::new("Launcher: tap for apps", Point::new(10, 40), text_style)
@@ -68,12 +64,10 @@ impl<'a> App<'a> { // Added lifetime parameter for 'a
     }
 
     fn show_settings(&mut self) -> Result<()> {
-        {
-            let mut touch = Touch::new_with_ref(self.i2c)?;
-            if let Some(event) = touch.read_event()? {
+            let mut touch = &mut self.touch;
+            if let Some(event) = touch.read_event()? { // Pass a mutable reference
                 if event.on_back() { self.state = AppState::Launcher; }
             }
-        }
         self.display.display.clear(Rgb565::BLACK);
         let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
         Text::new("Settings", Point::new(10, 40), text_style)
@@ -83,16 +77,14 @@ impl<'a> App<'a> { // Added lifetime parameter for 'a
     }
 
     fn show_battery(&mut self) -> Result<()> {
-        {
-            let mut power = PowerManager::new(self.i2c)?;
-            let voltage = power.read_voltage()?;
+            let voltage = self.power_manager.read_voltage()?;
             self.display.display.clear(Rgb565::BLACK);
             let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
             Text::new(&format!("Battery: {:.2} V", voltage / 1000.0), Point::new(10, 40), text_style)
                 .draw(&mut self.display.display);
-        }
-        let mut touch = Touch::new_with_ref(self.i2c)?;
-        if let Some(event) = touch.read_event()? { // This line was causing the issue
+
+        let mut touch = &mut self.touch;
+        if let Some(event) = touch.read_event()? { // Pass a mutable reference
             if event.on_back() { self.state = AppState::Launcher; }
         }
         FreeRtos::delay_ms(200);
